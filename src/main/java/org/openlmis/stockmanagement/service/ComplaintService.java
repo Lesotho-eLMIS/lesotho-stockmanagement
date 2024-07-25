1c172900d149d6ba1c46e705075623ec9ba5b3e5
khobatha <khobatha.setetemela@gmail.com>
2024-07-26 01:53:42 +0200

    restore to last working commit

--- src/main/java/org/openlmis/stockmanagement/service/ComplaintService.java
+++ src/main/java/org/openlmis/stockmanagement/service/ComplaintService.java
@@ -32,7 +32,6 @@ import org.openlmis.stockmanagement.dto.referencedata.RightDto;
 import org.openlmis.stockmanagement.dto.referencedata.UserDto;
 import org.openlmis.stockmanagement.exception.ResourceNotFoundException;
 import org.openlmis.stockmanagement.repository.ComplaintsRepository;
-import org.openlmis.stockmanagement.service.notification.NotificationService;
 import org.openlmis.stockmanagement.service.notifier.ComplaintNotifier;
 import org.openlmis.stockmanagement.service.referencedata.RightReferenceDataService;
 import org.openlmis.stockmanagement.service.referencedata.UserReferenceDataService;
@@ -256,27 +255,6 @@ public class ComplaintService {
       .build();
   }
 
-  /**
-   * Send a Complaint as a notification.
-   *
-   * @param id Commplaint id.
-   */
-  public void sendComplaint(UUID id) {
-    Optional<Complaint> optionalComplaint =  complaintsRepository.findById(id);
-    if (optionalComplaint.isPresent()) {
-      Complaint complaint = optionalComplaint.get();
-      RightDto right = rightReferenceDataService.findRight("STOCK_INVENTORIES_EDIT");
-      if (right != null) {
-        complaintNotifier.notifyRecipients(complaint, right.getId());
-      }
-      
-      //Notify NDSO
-      List<UserDto> supplierUsers = userReferenceDataService.findUsers("ndso");
-      if (supplierUsers != null) {
-        complaintNotifier.notifyRecipients(complaint, supplierUsers);
-      } 
-    }
-  }
   /**
    * Send a Complaint as a notification.
    *
