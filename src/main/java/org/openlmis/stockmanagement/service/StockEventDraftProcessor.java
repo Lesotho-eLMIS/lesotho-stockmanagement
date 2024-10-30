edf0caaa279a7ec1a0e4e669327ad9875831eae8
lmphatsi <la.mphatsi@gmail.com>
2024-10-30 12:21:25 +0200

    stock event draft service layer

---
+++ src/main/java/org/openlmis/stockmanagement/service/StockEventDraftProcessor.java
@@ -0,0 +1,98 @@
+/*
+ * This program is part of the OpenLMIS logistics management information system platform software.
+ * Copyright © 2017 VillageReach
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Affero General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details. You should have received a copy of
+ * the GNU Affero General Public License along with this program. If not, see
+ * http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org.
+ */
+
+package org.openlmis.stockmanagement.service;
+
+import java.util.UUID;
+import org.openlmis.stockmanagement.domain.eventdraft.StockEventDraft;
+import org.openlmis.stockmanagement.dto.StockEventDraftDto;
+import org.openlmis.stockmanagement.repository.StockEventsDraftRepository;
+import org.openlmis.stockmanagement.util.StockEventProcessContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.ext.XLogger;
+import org.slf4j.ext.XLoggerFactory;
+import org.slf4j.profiler.Profiler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * A service that is in charge of saving stock events and generating stock cards and line items from
+ * stock events.
+ */
+@Service
+public class StockEventDraftProcessor {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(StockEventDraftProcessor.class);
+  private static final XLogger XLOGGER = XLoggerFactory.getXLogger(StockEventDraftProcessor.class);
+
+  @Autowired
+  private StockEventDraftProcessContextBuilder contextBuilder;
+
+  @Autowired
+  private StockEventsDraftRepository stockEventsDraftRepository;
+
+
+  /**
+   * Validate and persist event and create stock card and line items from it.
+   *
+   * @param eventDto stock event dto.
+   * @return the persisted event ids.
+   */
+
+  public UUID process(StockEventDraftDto eventDto) {
+    XLOGGER.entry(eventDto);
+    Profiler profiler = new Profiler("PROCESS");
+    profiler.setLogger(XLOGGER);
+
+    profiler.start("BUILD_CONTEXT");
+    StockEventProcessContext context = contextBuilder.buildContext(eventDto);
+    eventDto.setContext(context);
+
+    //Disable validation since this is a draft ... some fields may be blank at the time
+    // profiler.start("VALIDATE");
+    // stockEventValidationsService.validate(eventDto);
+
+    UUID eventId = saveEventAndGenerateLineItems(
+        eventDto, profiler.startNested("SAVE_AND_GENERATE_LINE_ITEMS")
+    );
+
+    // StockEventPostProcessor stockEventPostProcessor = extensionManager.getExtension(
+    //     ExtensionPointId.STOCK_EVENT_POINT_ID, StockEventPostProcessor.class);
+    //stockEventPostProcessor.process(eventDto);
+
+    profiler.stop().log();
+    XLOGGER.exit(eventId);
+
+    return eventId;
+  }
+
+  private UUID saveEventAndGenerateLineItems(StockEventDraftDto eventDto, Profiler profiler) {
+    profiler.start("CONVERT_TO_EVENT");
+    StockEventDraft stockEvent = eventDto.toEvent();
+
+    profiler.start("DB_SAVE");
+    UUID savedEventId = stockEventsDraftRepository.save(stockEvent).getId();
+    LOGGER.debug("Saved stock event with id " + savedEventId);
+
+    // profiler.start("SAVE_FROM_EVENT");
+    // stockCardService.saveFromEvent(eventDto, savedEventId);
+
+    // profiler.start("CALL_NOTIFICATIONS");
+    // stockEventNotificationProcessor.callAllNotifications(eventDto);
+
+    return savedEventId;
+  }
+}
